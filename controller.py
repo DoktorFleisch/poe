f7410aa26321ac01b03aa18fc471d80eadeda9a9
Daniel Warkus <daniel.warkus@hhu.de>
2024-08-06 22:20:43 +0200

    making contact with league api

--- controller.py
+++ controller.py
@@ -1,1 +1,17 @@
-print('test')
+import requests
+
+#   response = requests.get("https://api.pathofexile.com/leagues", headers={"User-Agent": "Oauth mylearningapp/0.0 ("
+#                                                                                           "contact: "
+#                                                                                           "danielwarkus@hotmail.com)"})
+
+api_url =  "https://api.pathofexile.com"
+header = "User-Agent: Oauth mylearningapp/0.0 (contact: danielwarkus@hotmail.com"
+
+def get_leagues():
+    response = requests.get(api_url + "/leagues",
+                            headers={"User-Agent": "Oauth mylearningapp/0.0 (contact: ""danielwarkus@hotmail.com)"})
+    return response.json()
+
+if __name__ == '__main__':
+    leagues = get_leagues()
+    print(leagues)
