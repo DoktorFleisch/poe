aee6cc1999682b278b881adc66f31a2785fcccea
Daniel Warkus <daniel.warkus@hhu.de>
2024-08-06 22:25:33 +0200

    refactored

--- controller.py
+++ controller.py
@@ -1,15 +1,11 @@
 import requests
 
-#   response = requests.get("https://api.pathofexile.com/leagues", headers={"User-Agent": "Oauth mylearningapp/0.0 ("
-#                                                                                           "contact: "
-#                                                                                           "danielwarkus@hotmail.com)"})
-
 api_url =  "https://api.pathofexile.com"
-header = "User-Agent: Oauth mylearningapp/0.0 (contact: danielwarkus@hotmail.com"
+header = {"User-Agent": "Oauth mylearningapp/0.0 (contact: ""danielwarkus@hotmail.com)"}
 
 def get_leagues():
-    response = requests.get(api_url + "/leagues",
-                            headers={"User-Agent": "Oauth mylearningapp/0.0 (contact: ""danielwarkus@hotmail.com)"})
+    response = requests.get(api_url + "/leagues", headers=header)
+
     return response.json()
 
 if __name__ == '__main__':
